1ac1452846188a3fa1b1ccb38ce5f816b0c0e2ad
Akosua <addo.adofowaah@gmail.com>
2022-06-02 15:48:00 +0000

    number_10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * function that checks if list contains cycle
+ * Return 1 if list has cycle
+ * Return 0 if it does not
+ */
+int check_cyc(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+	if (!list)
+		return (0);
+	while (slow && fast && fast -> next)
+	{
+		slow = slow -> next;
+		fast = fast -> next -> next;
+		if (fast == slow)
+			return(1);
+	}
+	return (0);
+}
