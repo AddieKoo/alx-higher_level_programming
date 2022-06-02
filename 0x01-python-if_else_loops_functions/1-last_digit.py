c5e2e8d4b18abfe3d7e12c232f5d80a00aa6d3a7
Akosua <addo.adofowaah@gmail.com>
2022-06-02 15:12:47 +0000

    digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,10 @@ str2 = "is"
 str3 = "and is greater than 5"
 str4 = "and is 0"
 str5 = "and is less than 6 and not 0"
-ldt = abs(number) % 10
+if number > 0:
+    ldt = abs(number) % 10
+elif number < 0:
+    ldt = number % -10
 if ldt > 5:
     print(f"{str1} {number} {str2} {ldt} {str3}")
 elif ldt == 0:
